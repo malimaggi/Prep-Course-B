4b95f6c8cfe2d5cd7a485a90c78135c5b9af080c
malimaggi <you@example.com>
2022-11-30 18:58:10 -0300

    Costo pero salio, sigue asi

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,46 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma = 0 ;
+    for (var i = 0 ; i < 11 ; i++)
+    {suma = suma + i  } 
+    return suma 
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+ return array.filter ((element) => element %2 === 0)
+return filtrados 
+
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map((element) => element ** 2 ) ;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+
+ return array.reduce((anterior,siguiente) => anterior+siguiente)
+
+
+    
+ 
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var numenstring = num.toString()
+  var longitud = numenstring.length ;
+  return longitud 
 }
   
   
